ec1d3fe5c70eccdb1a51f3340acec0b495d4fd18
leandrocl2005 <leandrocl2005@yahoo.com.br>
2019-02-03 11:19:58 -0200

    initial

---
+++ README.md
@@ -0,0 +1,66 @@
+# Log analysis with postgres SQL and Python
+
+In this project we'll build a data reporting on a postgres database with a log table using *Python 3.5.2* and *psycopg2* library.
+
+## Installation of virtual-machine
+
+First, we need to create our virtual machine with all settings for this project:
+
+* Install Oracle VM VirtualBox
+* Install Vagrant
+
+Replace the *newsdata.sql* in *newsdata* folder by the *newsdata.sql* provide by Udacity in this link: https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
+
+Enter with the commands in Git Bash:
+
+```sh
+$ git clone https://github.com/leandrocl2005/SQL_project3_udacity_fullstack.git
+$ cd SQL_project3_udacity_fullstack/
+$ cd vagrant
+$ vagrant up
+$ vagrant ssh
+```
+
+If all things are right, we are in our VM!
+
+## Creating the news database
+
+Let's create our database:
+
+```sh
+$ cd ..
+$ cd ..
+$ cd vagrant
+$ psql -d news -f newsdata/newsdata.sql
+```
+
+The dataset contains 3 tables: articles, authors and log. To see it, in Git Bash enter:
+
+```sh
+$ psql -d news
+news => \dt
+```
+
+To exit from psql, just press Ctrl+D. 
+
+## The analysis
+
+We want to answer 3 questions about the tables in our database (the questions are in pt-br language):
+
+* "Quais são os três artigos mais populares de todos os tempos?"
+* "Quem são os autores de artigos mais populares de todos os tempos?"
+* "Em quais dias mais de 1% das requisições resultaram em erros?"
+
+To do that, just tip in Git Bash the following:
+
+```sh
+> python3 main.py
+```
+
+The result is like *result.txt* file.
+
+# Credits
+
+  - the *news.sql* and the vagrant setup files were provided by Udacity
+
+
---
+++ vagrant/main.py
@@ -0,0 +1,82 @@
+import psycopg2
+
+
+def get_query_result(query):
+    db = psycopg2.connect(database="news")
+    c = db.cursor()
+    c.execute(query)
+    result = c.fetchall()
+    db.close()
+    return result
+
+
+def print_breaks():
+    print()
+    print("########==============================================########")
+    print()
+
+query = """
+SELECT title_path.t, count(log.path) AS c
+FROM
+    (SELECT DISTINCT
+        articles.title AS t,
+        log.path AS p
+    FROM
+        articles
+    LEFT JOIN log ON log.path ILIKE '%' || articles.slug || '%') AS title_path
+LEFT JOIN log on log.path = title_path.p
+GROUP BY title_path.t
+ORDER BY c DESC
+LIMIT(3);
+"""
+
+print("Quais são os três artigos mais populares de todos os tempos?")
+print()
+print("{:>35s}{:>20s}".format("Título", "Nº de acessos"))
+for row in get_query_result(query):
+    print("{:>35s}{:>20d}".format(row[0], row[1]))
+print_breaks()
+
+query = """
+SELECT slug_name.aun, count(log.path) AS c
+FROM
+    (SELECT
+        articles.slug AS ars,
+        authors.name AS aun
+    FROM
+        authors
+    LEFT JOIN articles
+    ON authors.id = articles.author) AS slug_name
+LEFT JOIN log on log.path ILIKE '%' || slug_name.ars || '%'
+GROUP BY slug_name.aun
+ORDER BY c DESC
+LIMIT(3);
+"""
+
+print("Quem são os autores de artigos mais populares de todos os tempos?")
+print()
+print("{:>35s}{:>20s}".format("Author", "Nº de acessos"))
+for row in get_query_result(query):
+    print("{:>35s}{:>20d}".format(row[0], row[1]))
+print_breaks()
+
+query = """
+SELECT *
+FROM
+    (SELECT
+        to_char(date_trunc('day', time), 'DD Mon YYYY'),
+        SUM(
+            CASE WHEN status = '404 NOT FOUND' THEN 1 ELSE 0 END
+        )::DECIMAL / COUNT(status) AS percent_total
+    FROM log
+    GROUP BY 1
+    ORDER BY 1) AS temp_table
+WHERE percent_total > 0.01;
+"""
+
+print("Em quais dias mais de 1% das requisições resultaram em erros?")
+print()
+print("{:>15s}{:>20s}".format("DATA", "PORCENTAGEM (%)"))
+result = get_query_result(query)[0]
+print("{:>15s}{:>19.4f}%".format(result[0], 100*float(result[1])))
+print_breaks()
